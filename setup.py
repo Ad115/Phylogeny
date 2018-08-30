5b386b4c08568af7cd9e55aae6fe35cdae5f4a02
Ad115 <a.garcia230395@gmail.com>
2018-08-30 03:07:15 -0500

    Added NetworkX to dependencies O.o

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ete3', 'matplotlib', 'numpy'],  # Optional
+    install_requires=['ete3', 'matplotlib', 'numpy', 'networkx'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
