af440b20987aa677b0ac50795a8f4f6a2956dd1c
Ad115 <a.garcia230395@gmail.com>
2018-08-30 03:03:49 -0500

    Fixed typo on import

--- phylogeny/reconstruction/__init__.py
+++ phylogeny/reconstruction/__init__.py
@@ -1,3 +1,3 @@
-from .allquartets import all_quartets_method, four_points_method
+from .allquartets import all_quartets_method, four_point_method
 from .clocklike import infer_clocklike_tree
 from .ultrametric import ultrametric_to_tree
